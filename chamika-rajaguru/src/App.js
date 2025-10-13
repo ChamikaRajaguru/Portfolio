7400e6bbab47f8ac6839b9f122a5563b42044022
Chamika Rajaguru <chamikarajaguru1@gmail.com>
2025-10-14 00:55:36 +0530

    update code

--- chamika-rajaguru/src/App.js
+++ chamika-rajaguru/src/App.js
@@ -1,12 +1,11 @@
-import './App.css';
-import Navbar from './Components/Navbar';
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <>
-    
-        
     <Navbar />
+        
+  
     </>
     
   );
---
+++ chamika-rajaguru/src/Components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+function Navbar(){
+
+    return(
+   <div>     
+        <ul className="nav-links" id="navLinks">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#skills">Skills</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+   </div>     
+
+    );
+}
+
+export default Navbar;
